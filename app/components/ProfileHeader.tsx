8d9173728f5e5960ed09f0bb17478ecf21a1e5cf
Gustavo <carlosgsp39@gmail.com>
2025-05-02 01:39:08 +0100

    tweak tweak tweak raul

--- app/components/ProfileHeader.tsx
+++ app/components/ProfileHeader.tsx
@@ -14,7 +14,7 @@ export default function ProfileHeader({
   outfitCount,
 }: ProfileHeaderProps) {
   return (
-    <div className="flex flex-col sm:flex-row items-center sm:items-end gap-4 sm:gap-6 p-4 md:p-6">
+    <div className="flex flex-col sm:flex-row items-center sm:items-center gap-4 sm:gap-6 p-4 md:p-6">
       <ResponsiveAvatar imageSrc={imageSrc} />
 
       <div className="text-center sm:text-left">
